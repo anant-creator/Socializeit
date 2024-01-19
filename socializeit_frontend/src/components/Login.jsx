f070f98b2b518120e8b31f09434c30aa5580452b
Anant Gupta <61108547+anant-creator@users.noreply.github.com>
2024-01-19 23:38:21 +0530

    logo change

--- socializeit_frontend/src/components/Login.jsx
+++ socializeit_frontend/src/components/Login.jsx
@@ -3,7 +3,7 @@ import { GoogleLogin } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
 // import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/share.mp4";
-import logo from "../assets/logowhite.png";
+import logo from "../assets/logo.png";
 import { client } from "../client";
 import jwt_decode from 'jwt-decode';
 
